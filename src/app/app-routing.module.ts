5f9a663644d0973c2af7e35067e3e21e2fcdb3cf
Sebastián Herrera <al233324@edu.uaa.mx>
2024-05-22 00:54:23 -0600

    Maldita sea, js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'contacto', component: ContactoComponent },
-  { path: 'cualidades', component: CualidadesComponent }
+  { path: '', component: CualidadesComponent }
 ];
 
 @NgModule({
